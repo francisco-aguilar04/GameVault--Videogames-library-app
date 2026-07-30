ee28ba5aeafb7dfe599d3f030257412ab60f75eb
Francisco Aguilar <pacoaguilarcid@gmail.com>
2026-07-30 12:50:54 +0200

    feat: add Game model

---
+++ backend-go/internal/models/gameModel.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Game struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Rating      *float64  `json:"rating"`
+	Review      *string   `json:"review"`
+	Status      string    `json:"status"`
+	ReleaseYear *int      `json:"release_year"`
+	PhotoURL    *string   `json:"photo_url"`
+	CreatedAt   time.Time `json:"created_at"`
+	PlatformIDs []int     `json:"platform_ids"`
+}
